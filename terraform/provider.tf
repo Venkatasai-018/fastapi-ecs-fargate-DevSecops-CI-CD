baf18d56cc1613b4e05346e55aa716d824e38cd6
venkatasaiKommu <Venkatasai.Kommu@Kroll.com>
2026-02-22 16:06:18 +0530

    added terrform code

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0"
+    }
+  }
+  backend "s3" {
+    bucket = "not-bucket-123"
+    key    = "terraform.tfstate"
+    region = "ap-south-2"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,52 @@
+resource "aws_vpc" "fvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+        Name = "fastapi-vpc"
+    }
+}
+
+resource "aws_subnet" "fsubnet1" {
+  vpc_id = aws_vpc.fvpc.id
+  cidr_block = "10.0.0.1/24"
+  availability_zone = "ap-south-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "fastapi-subnet-1"
+   }
+}
+
+resource "aws_subnet" "fsubnet2" {
+  vpc_id = aws_vpc.fvpc.id
+  cidr_block = "10.0.0.2/24"
+  availability_zone = "ap-south-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "fastapi-subnet-2"
+   }
+}
+
+
+resource "aws_internet_gateway" "figw" {
+  vpc_id = aws_vpc.fvpc.id
+  tags = {
+    Name = "fastapi-igw"
+  }
+}
+
+resource "aws_route_table" "frtb" {
+  vpc_id = aws_vpc.fvpc.id
+  tags = {
+    Name = "fastapi-rtb"
+  }
+}
+
+resource "aws_route_table_association" "frtb_assoc1" {
+  subnet_id = aws_subnet.fsubnet1.id
+  route_table_id = aws_route_table.frtb.id
+}
+
+resource "aws_route_table_association" "frtb_assoc2" {
+  subnet_id = aws_subnet.fsubnet2.id
+  route_table_id = aws_route_table.frtb.id
+}
+ 
